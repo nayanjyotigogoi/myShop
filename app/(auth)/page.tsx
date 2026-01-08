f34ab9e1e8407127394ec6a6cc014534d50fd91f
Nayanjyoti Gogoi <90410611+nayanjyotigogoi@users.noreply.github.com>
2026-01-09 01:04:08 +0530

    Update page.tsx

--- app/(auth)/page.tsx
+++ app/(auth)/page.tsx
@@ -26,7 +26,7 @@ export default function LoginPage() {
     setError(null)
 
     try {
-      const res = await fetch("http://127.0.0.1:8000/api/login", {
+      const res = await fetch("https://adkaro.online/api/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
